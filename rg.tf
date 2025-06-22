c3eb69eb7774e7a243f1cdece6dde5b38a8d6bfe
manoj <manoj.thadiparthi554@gmail.com>
2025-06-22 18:08:32 +0530

    add

--- rg.tf
+++ rg.tf
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 resource "azurerm_resource_group" "rg01" {
    name = "az-rg-01" 
    location = "centralindia"
    tags = {
    owner = "vcube"
    }
+=======
+resource "azure_resource_group" "rg01" {
+    name = "az-rg-01"
+    location = "centralindia"
+  
+>>>>>>> parent of fded8a8 (added code)
 }
